86c3b055d6e85a45128bf214f6ce47079a518c32
Jayson Olaguera <jayson.olaguera@eyeball.club>
2025-10-08 16:36:59 +0800

    header

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,3 +1,13 @@
-export async function GET() {
-    return new Response('Hello from ProfilePage!');
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+    // const requestHeaders = new Headers(request.headers);
+    // console.log(requestHeaders.get("Authorization"));
+
+    const headerList = await headers();
+    console.log(headerList.get("Authorization"));
+    return new Response('<h1>Hello from Profile Page!</h1>', {
+        headers: { "Content-Type": "text/html" }
+    });
 }
