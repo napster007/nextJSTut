865b7cfded687f4a255e00cb7581b234940a3984
Jayson Olaguera <jayson.olaguera@eyeball.club>
2025-10-08 16:49:54 +0800

    cookies

--- route-handlers-demo/src/app/profile/api/route.ts
+++ route-handlers-demo/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
     // const requestHeaders = new Headers(request.headers);
@@ -7,7 +7,15 @@ export async function GET(request: NextRequest) {
 
     const headerList = await headers();
     console.log(headerList.get("Authorization"));
+    const theme = request.cookies.get("theme");
+    console.log("Theme cookie:", theme);
+    const cookieStore = await cookies();
+    cookieStore.set("resultsPerPage", "20");
+    console.log(cookieStore.get("resultsPerPage"))
     return new Response('<h1>Hello from Profile Page!</h1>', {
-        headers: { "Content-Type": "text/html" }
+        headers: {
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
+        }
     });
 }
